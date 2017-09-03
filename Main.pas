6ad1f6f109bbda9063051385d1240585ad592c57
luxidea <contact@luxidea.net>
2017-09-04 03:58:18 +0900

    begin

---
+++ Main.pas
@@ -0,0 +1,26 @@
+﻿unit Main;
+
+interface //#################################################################### ■
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  LUX.GPU.OpenGL.Viewer;
+
+type
+  TForm2 = class(TForm)
+    GLViewer1: TGLViewer;
+  private
+    { private 宣言 }
+  public
+    { public 宣言 }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation //############################################################### ■
+
+{$R *.fmx}
+
+end. //######################################################################### ■
