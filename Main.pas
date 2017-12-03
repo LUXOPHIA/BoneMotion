a5943b27a2d4cada15827a12b02e7612750aa2c3
luxidea <contact@luxidea.net>
2017-12-04 01:15:46 +0900

    no message

--- Main.pas
+++ Main.pas
@@ -5,7 +5,7 @@ interface //####################################################################
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
-  LUX, LUX.D1, LUX.D2, LUX.D3, LUX.M4,
+  LUX, LUX.D1, LUX.D2, LUX.D3, LUX.M4,
   LUX.GPU.OpenGL.Viewer,
   LUX.GPU.OpenGL.Camera,
   LUX.GPU.OpenGL.Scener,
